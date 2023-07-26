290bca2a70a4bcdd9847c4a8778ff176fab9b079
afratetlay <afratetlay@gmail.com>
2023-07-26 14:42:16 +0200

    set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 
 // import aos
-// import Aos from "aos";
+import Aos from "aos";
 // import aos css
-// import "aos/dist.aos.css";
 
 // import components
 import Header from "./components/Header";
@@ -20,11 +19,11 @@ import Cta from "./components/Cta";
 import Footer from "./components/Footer";
 
 const App = () => {
-  // initialize aos
-  // Aos.init({
-  //   duration: 1800,
-  //   offset: 100,
-  // });
+  //initialize aos
+  Aos.init({
+    duration: 1800,
+    offset: 100,
+  });
   return (
     <div className="overflow-hidden">
       <Header />
