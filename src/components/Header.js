c4c4a6b9556ab3c94ec98cc8ca97b717b008f089
afratetlay <afratetlay@gmail.com>
2023-08-08 16:54:38 +0200

    working on the header section content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+// import data
+import { header } from "../data";
+//import icons
+import { HiMenuAlt4, HiOutlineX } from "react-icons/hi";
+// import components
+import MobileNav from "../components/MobileNav";
+import Nav from "../components/Nav";
 
 const Header = () => {
-  return <div>Header</div>;
+  // mobile nav state
+  const [MobileNav, setMobileNav] = useState(false);
+  // header state
+  const [isActive, setIsActive] = useState(false);
+  // destructure header data
+  const { logo, btnText } = header;
+  // scroll event
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
+    });
+  });
+
+  return (
+    <header
+      className={`${
+        isActive ? "lg:top-0 bg-white shadow-2xl" : "lg:top-[60px]"
+      } py-6 lg:py-4 fixed w-full transition-all z-10`}
+    >
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <a href="#">
+          <img src={logo} alt="" />
+        </a>
+        {/* Nav - initially hidden - shown on desktop mode */}
+        <div className="hidden lg:flex">
+          <Nav />
+        </div>
+        {/* CTA button - initially hidden - show on desktop mode*/}
+        <button className="btn btn-sm btn-outline hidden lg:flex">
+          {btnText}
+        </button>
+        {/* mobile nav trigger btn - hidden on desktop */}
+        <button className="lg:hidden" onClick={() => setMobileNav(!MobileNav)}>
+          {MobileNav ? (
+            <HiOutlineX className="text-3xl text-accent" />
+          ) : (
+            <HiMenuAlt4 className="text-3xl text-accent" />
+          )}
+        </button>
+        {/* mobile nav - hidden on desktop */}
+        {/* <div
+          className={`fixed top-0 bottom-0 w-[60vw] lg:hidden transition-all bg-pink-400`}
+        >
+          <MobileNav />
+        </div> */}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
