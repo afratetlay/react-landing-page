f7c14dcd87629b0eda607d0c9dc647a0e20f07b0
afratetlay <afratetlay@gmail.com>
2023-08-14 10:33:01 +0200

    adding the brands to the page

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -4,13 +4,20 @@ import { overview } from "../data";
 
 const Brands = () => {
   //destructure overview
-  const brands = overview;
+  const { brands } = overview;
   return (
-    <section className="py-9 bg-pink-100">
-      <div className="container mx-auto">
+    <section className="py-9">
+      <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between space-y-6">
         {/* brands */}
         {brands.map((item, index) => {
+          //destructure item
           const { image, delay } = item;
+          return (
+            <div key={index} data-aos="fade-up" data-aos-delay={delay}>
+              {/* brand img */}
+              <img src={image} alt="" />
+            </div>
+          );
         })}
       </div>
     </section>
