5ceb2b8d4b4989d3ae2fd4525d848c68ac3ccebc
afratetlay <afratetlay@gmail.com>
2023-08-30 10:22:53 +0200

    working on the testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+// import data
+import { testimonials } from "../data";
+// import components
+import ClientSlider from "../components/ClientSlider";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  //destructure testimonials
+  const { title, clients } = testimonials;
+  return (
+    <section className="bg-pink-200 section">
+      <div className="container mx-auto">
+        {/* title */}
+        <h2 className="title mb-10 lg:mb-20 text-center max-w-[920px] mx-auto">
+          {title}
+        </h2>
+        {/* slider */}
+        <div>
+          <ClientSlider clients={clients} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
