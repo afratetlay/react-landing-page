12c556e1fdaafe90a1f9bb667fdbde7a3a722a26
afratetlay <afratetlay@gmail.com>
2023-08-30 11:34:36 +0200

    working on testimonials and clientslider

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -8,14 +8,18 @@ const Testimonials = () => {
   //destructure testimonials
   const { title, clients } = testimonials;
   return (
-    <section className="bg-pink-200 section">
+    <section className="section">
       <div className="container mx-auto">
         {/* title */}
-        <h2 className="title mb-10 lg:mb-20 text-center max-w-[920px] mx-auto">
+        <h2
+          className="title mb-10 lg:mb-20 text-center max-w-[920px] mx-auto"
+          data-aos="fade-up"
+          data-aos-delay="200"
+        >
           {title}
         </h2>
         {/* slider */}
-        <div>
+        <div data-aos="fade-up" data-aos-delay="400">
           <ClientSlider clients={clients} />
         </div>
       </div>
